1930e67c527aca8bc69daf1946c8e72ef2d5a38c
Justin Truong <satruongjustin@gmail.com>
2022-10-19 18:55:23 -0500

    updated packages and added packages.config file

--- horoscopeApp/horoscopeApp/main.aspx.cs
+++ horoscopeApp/horoscopeApp/main.aspx.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.IO;
-using System.Net;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
 
 
 namespace horoscopeApp
