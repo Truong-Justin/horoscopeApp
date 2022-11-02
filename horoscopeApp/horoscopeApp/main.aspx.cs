7a19e45efbfcfa02be06fd81e2a2b482bcc4c191
Justin Truong <satruongjustin@gmail.com>
2022-11-01 20:07:23 -0500

    troubleshooting a runtime error after deployment

--- horoscopeApp/horoscopeApp/main.aspx.cs
+++ horoscopeApp/horoscopeApp/main.aspx.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.NetworkInformation;
 
 
 
@@ -19,16 +18,6 @@ namespace horoscopeApp
                 paragraph.InnerText = (string)api["data"];
             }
 
-            else
-            {
-                //Ping horoscope api to wake it up from sleep;
-                //Api is hosted on student free-tier account that 
-                //puts container to sleep after a certain amount
-                //of inactivity;
-                Ping myPing = new Ping();
-                PingReply reply = myPing.Send("129.7.81.50", 5000);
-
-            }
         }
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
